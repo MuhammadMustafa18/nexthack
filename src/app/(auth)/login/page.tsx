c9bdc490c0ae1ee11cde4f22264a4238d0ecd152
MuhammadMustafa18 <mustafatariq365@protonmail.com>
2025-10-02 16:05:10 +0500

    login improved

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -4,7 +4,7 @@ import { LoginForm } from "./components/loginForm";
 
 export default function LoginPage() {
   return (
-    <div className="flex flex-row space-y-2 px-4 justify-center items-center min-h-screen w-full">
+    <div className="flex flex-col space-y-2 px-4 justify-center items-center min-h-screen w-full">
       <LoginForm />
       <div className="w-full max-w-md mx-auto mb-4 p-3 text-sm text-blue-800 bg-blue-50 border border-blue-200 rounded-md">
         <strong>Note:</strong> If you signed up using your email earlier, please check
